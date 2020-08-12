0170e3d689c78f1589781b67363ba4776a9c22f0
VirtuosoEric <eric@virtuosorobotics.com>
2020-08-12 17:05:32 +0800

    test ma

--- web_joystick.py
+++ web_joystick.py
@@ -2,31 +2,68 @@
 from flask import Flask, render_template, session, request, flash
 from flask_socketio import SocketIO, emit 
 from Keyence_PLC_Ethernet import Keyence_PLC_Ethernet
-import time,threading
+import time,threading,random
 
 class BaseDriver:
     
     def __init__(self):
-        self.max_throttle = 100
+        self.max_throttle = 50
         self.max_steering = 20
         self.last_time = 0
+        self.steering_cmd = 0.0
+        self.throttle_cmd = 0.0
+        self.steering_target = 0.0
+        self.throttle_target = 0.0
 
     def connect_plc(self,ip,port):
         print ('connect to plc')
+        self.id = random.randint(0,99)
         self.plc = Keyence_PLC_Ethernet(ip,port)
+        t = threading.Thread(target=self.control_loop)
+        t.daemon = True
+        t.start()
+
+    def ma(self,steering,throttle):
+        self.steering_cmd = self.steering_cmd*0.8 + steering*0.2
+        self.throttle_cmd = self.throttle_cmd*0.8 + throttle*0.2
+        # print ('throttle_cmd '+ str(self.throttle_cmd)+' steering_cmd '+str(self.steering_cmd))
+
+    def update_target(self,joy_x,joy_y):
+        steering = -(joy_x/100) * self.max_steering
+        throttle = (joy_y/100) * self.max_throttle
+        self.steering_target = steering
+        self.throttle_target = throttle
+        # print ('throttle_target '+ str(self.throttle_target)+' steering_target '+str(self.steering_target))
+        
+    def control_loop(self):
+        last_loop_time = time.time()
+        while True:
+            time_now = time.time()
+            time_elast = time_now - last_loop_time
+            if time_elast > 0.3:
+                self.ma(self.steering_target,self.throttle_target)
+                data = [str(int(self.throttle_cmd)),str(int(self.steering_cmd))]
+                print(data)
+                self.plc.consecutive_write_data('DM','101','.L',data)
+                # print(self.id)
+                last_loop_time = time_now
+                
 
     def send_cmd(self,joy_x,joy_y):
         steering = -(joy_x/100) * self.max_steering
         throttle = (joy_y/100) * self.max_throttle
         time_now = time.time()
         time_elast = time_now - self.last_time
-        if time_elast > 0.3:
+        if time_elast > 0.2:
+            self.ma(steering,throttle)
             print ('throttle '+ str(throttle)+' steering '+str(steering))
             data = [str(int(throttle)),str(int(steering))]
             self.plc.consecutive_write_data('DM','101','.L',data)
             self.last_time = time_now
+            print(time_elast)
         else:
-            print('waiting')
+            pass
+            # print('waiting')
 
 driver = BaseDriver()
 
@@ -57,7 +94,8 @@ def joystick_cmd(msg):
         throttle = 100
     elif throttle < -100:
         throttle = -100
-    driver.send_cmd(steering,throttle)
+    # driver.send_cmd(steering,throttle)
+    driver.update_target(steering,throttle)
 
 
 
